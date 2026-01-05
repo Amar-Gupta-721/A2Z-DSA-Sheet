0e8118f6cda0e2100c3c523a132338eae794dd07
Amar-Gupta-721 <amargupta721@gmail.com>
2026-01-05 13:43:29 +0530

    updated pattern 11

--- patterns/pattern11.java
+++ patterns/pattern11.java
@@ -2,6 +2,12 @@ package patterns;
 
 public class pattern11 {
     public static void main(String[] args) {
+        // Pattern 11
+        // 1
+        // 01
+        // 101
+        // 0101
+        // 10101
         for(int i=0; i<5; i++){
             for(int j=0; j<=i; j++){
                 if((i+j)%2!=0)System.out.print(0);
